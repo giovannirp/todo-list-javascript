2f600fc04d9118805dedfdea3d98b7576dd2000b
giovanni <giovanni_ge@hotmai.com>
2023-06-01 11:35:44 -0300

    feat: adicionando função de filter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const todosContainer = document.querySelector(".todos-container");
 const formAddTodoList = document.querySelector(".form-add-todo");
+const formSearchInput = document.querySelector(".form-search input");
+const formSearch = document.querySelector(".form-search input");
 
 formAddTodoList.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -22,4 +24,23 @@ todosContainer.addEventListener("click", (event) => {
   if (Array.from(itemClick.classList).includes("delete")) {
     itemClick.parentElement.remove()
   }
+});
+
+formSearch.addEventListener("input", (event) => {
+  const inputValue = event.target.value.toLowerCase().trim();
+  
+  Array.from(todosContainer.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach((item) => {
+      item.classList.add('hidden');
+      item.classList.remove('d-flex');
+  });
+
+  Array.from(todosContainer.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach((item) => {
+      item.classList.remove('hidden');
+      item.classList.add('d-flex');
+  });
 })
+
