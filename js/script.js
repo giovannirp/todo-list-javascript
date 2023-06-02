675c4557f47178ac48c9f04679b5cd823d60f2fc
giovanni <giovanni_ge@hotmai.com>
2023-06-02 15:59:53 -0300

    fix: refatorando codigo

--- js/script.js
+++ js/script.js
@@ -3,10 +3,7 @@ const formAddTodoList = document.querySelector(".form-add-todo");
 const formSearchInput = document.querySelector(".form-search input");
 const formSearch = document.querySelector(".form-search input");
 
-formAddTodoList.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const inputValue = event.target.add.value.trim();
-
+const addTodoList = (inputValue) => {
   if (inputValue.length) {
     todosContainer.innerHTML += `
     <li class="list-group-item d-flex justify-content-between align-items-center" data-todo=${inputValue}>
@@ -16,16 +13,28 @@ formAddTodoList.addEventListener("submit", (event) => {
   }
 
   event.target.reset();
+}
+
+formAddTodoList.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputValue = event.target.add.value.trim();
+  addTodoList(inputValue);
 });
 
-todosContainer.addEventListener("click", (event) => {
-  const itemClick = event.target;
-  const todo = document.querySelector(`[data-todo="${itemClick.dataset.trash}"]`);
-  const trashDataValue = itemClick.dataset.trash;
+const removeTodo = (itemClickedElement) => {
+  const trashDataValue = itemClickedElement.dataset.trash;
+  const todo = document.querySelector(`[data-todo="${itemClickedElement.dataset.trash}"]`);
 
   if (trashDataValue) {
     todo.remove();
   }
+}
+
+todosContainer.addEventListener("click", (event) => {
+  const itemClick = event.target;
+
+  removeTodo(itemClick);
 });
 
 formSearch.addEventListener("input", (event) => {
